67d07aa9758c763d8f98c783e68c418425a692da
Demianenko Konstantin <dem.konst@yandex.ru>
2021-05-14 23:20:51 +0200

    Context presentation cleanup

--- context/js/context.js
+++ context/js/context.js
@@ -1,48 +1,48 @@
-if (window.story === undefined) {
-    window.story = {
-        app: {
-            // ...
-        },
-        // ...
-        questProgress: {
-            id: "1",
-            field1: 'Hello World',
-            field2: '2 field',
-            field3: true,
-            field4: 123,
-            objField5: {
-                id: "2",
-                field51: '5 field',
-                field52: false,
-                field53: null,
-                objField54: {
-                    id: "3",
-                    field541: '541_field1',
-                    field542: 22345,
-                    field543: undefined
+// rev:1
 
-                }
-            }
-        }
-    };
-};
-if (window.changeStoryObject !== undefined) {
-    console.log('changeStoryObject exists');
-    if (window.changeStoryObject.changeStory !== undefined) {
-        console.log('changeStory exists');
-        console.log(JSON.stringify(window.story));
-        window.changeStoryObject.changeStory(JSON.stringify(window.story));
-    } else {
-        console.log('changeStory does not exist');
-    }
-} else {
-    console.log('changeStoryObject does not exist');
-};
+// if (window.story === undefined) {
+//     window.story = {
+//         app: {
+//             // ...
+//         },
+//         // ...
+//         questProgress: {
+//             id: "1",
+//             field1: 'Hello World',
+//             field2: '2 field',
+//             field3: true,
+//             field4: 123,
+//             objField5: {
+//                 id: "2",
+//                 field51: '5 field',
+//                 field52: false,
+//                 field53: null,
+//                 objField54: {
+//                     id: "3",
+//                     field541: '541_field1',
+//                     field542: 22345,
+//                     field543: undefined
 
-var onStoryChange = function () {
-    console.log('onStoryChange');
-    console.log(JSON.stringify(window.story));
-}
+//                 }
+//             }
+//         }
+//     };
+// };
+
+// if (window.changeStoryObject !== undefined) {
+//     console.log('changeStoryObject exists');
+//     if (window.changeStoryObject.changeStory !== undefined) {
+//         console.log('changeStory exists');
+//         console.log(JSON.stringify(window.story));
+//         window.changeStoryObject.changeStory(JSON.stringify(window.story));
+//     } else {
+//         console.log('changeStory does not exist');
+//     }
+// } else {
+//     console.log('changeStoryObject does not exist');
+// };
+
+
 
 var StateRaw = {
     setState: function (parentId, key, value) {
@@ -66,7 +66,6 @@ var proxify = function (obj) {
     let setter = function (target, property, value, receiver) {
         console.log(`setter ${property}`);
         target[property] = value;
-        //$('#state').text(JSON.stringify(window.State, null, 4));
         return true;
     };
 
@@ -86,13 +85,13 @@ var proxify = function (obj) {
 };
 
 ; (function () {
-    window.State = proxify(StateRaw.data);
-    console.log(window.State);
-    console.log(`Current state in IIFE: ${JSON.stringify(window.State)}`);
+    // window.State = proxify(StateRaw.data);
+    // console.log(window.State);
+    // console.log(`Current state in IIFE: ${JSON.stringify(window.State)}`);
 
-    window.State.field1 = 'test Hello World';
-    // window.State.objField5.field51 = 'test Hello World';
-    // window.State.objField5.field55 = 'test field555';
+    // window.State.field1 = 'test Hello World';
+    // // window.State.objField5.field51 = 'test Hello World';
+    // // window.State.objField5.field55 = 'test field555';
 
-    console.log(window.State);
+    // console.log(window.State);
 })();
--- context/js/index.js
+++ context/js/index.js
@@ -1,105 +1,111 @@
+$('#state').text(JSON.stringify(window.story, null, 4));
 
-// LAB START
-; (function () {
-    let getter = function (target, property, receiver) {
-
-        // TODO: some logic
-
-        console.log("getter: " + property);
-        console.log("getter path: " + target.path);
-        return target[property];
-    };
-    let setter = function (target, property, value, receiver) {
-
-        // TODO: some logic
-
-        console.log("setter of: " + property + " value: " + value);
-        console.log("setter path: " + target.path);
-        target[property] = value;
-        return true;
-    };
-
-    let nestedProxy = function (target, path = []) {
-        console.log(path);
-        for (targetProp in target) {
-            if (typeof target[targetProp] === 'object') {
-                path.push(targetProp);
-                target[targetProp] = nestedProxy(target[targetProp], path);
-
-            }
-        }
-
-        let result = new Proxy(target, {
-            get: getter,
-            set: setter
-        });
-        result.path = path.join(".");
-        return result;
-    }
-
-    let proxyTarget = {
-        prop1: "val1",
-        prop2: {
-            prop3: "val2",
-            prop4: "val3"
-        }
-    };
-
-    let proxy = new nestedProxy(proxyTarget, ["root"]);
-    var v = proxy.prop1; // getter test
-    var v1 = proxy.prop2.prop3; // getter test
-
-    proxy.prop1 = "set1"; // setter test
-    proxy.prop2.prop3 = "set2"; //setter test
-    proxy.prop5 = "val4"; //setter test
-
-    console.log(proxy);
-
-})();
-
-// LAB FINISh
-
-var storyContext = window.story;
-console.log(window.Native);
-
-if (!storyContext) {
-    storyContext = {
-        accentColor: {
-            color: "blue"
-        }
-
-    }
+var onStoryChange = function () {
+    // console.log('onStoryChange');
+    // console.log(JSON.stringify(window.story));
+    $('#state').text(JSON.stringify(window.story, null, 4));
 }
 
-var logState = function () {
-    if (!storyContext) {
-        console.log("no storyContext");
-        return
-    }
-    $("body").css("background-color", storyContext.accentColor.color);
-    console.log(JSON.stringify(storyContext.State, 4));
-}
+// ; (function () {
+//     let getter = function (target, property, receiver) {
 
-var TestFunc2 = function () {
-    if (!storyContext) {
-        console.log("no context");
-        return
-    }
-    let val = $("input[name='testInput2']").val();
-    console.log(val);
+//         // TODO: some logic
 
-    if (storyContext != undefined) {
-        var result = context.TestMethod2(val);
-        $("#result").val(result);
-    }
+//         console.log("getter: " + property);
+//         console.log("getter path: " + target.path);
+//         return target[property];
+//     };
+//     let setter = function (target, property, value, receiver) {
 
-    return result;
-}
+//         // TODO: some logic
 
-var Native = {
-    TestCall1: function () {
-        return 123;
-    }
-}
+//         console.log("setter of: " + property + " value: " + value);
+//         console.log("setter path: " + target.path);
+//         target[property] = value;
+//         return true;
+//     };
 
-Native.TestCall1();
+//     let nestedProxy = function (target, path = []) {
+//         console.log(path);
+//         for (targetProp in target) {
+//             if (typeof target[targetProp] === 'object') {
+//                 path.push(targetProp);
+//                 target[targetProp] = nestedProxy(target[targetProp], path);
+
+//             }
+//         }
+
+//         let result = new Proxy(target, {
+//             get: getter,
+//             set: setter
+//         });
+//         result.path = path.join(".");
+//         return result;
+//     }
+
+//     let proxyTarget = {
+//         prop1: "val1",
+//         prop2: {
+//             prop3: "val2",
+//             prop4: "val3"
+//         }
+//     };
+
+//     let proxy = new nestedProxy(proxyTarget, ["root"]);
+//     var v = proxy.prop1; // getter test
+//     var v1 = proxy.prop2.prop3; // getter test
+
+//     proxy.prop1 = "set1"; // setter test
+//     proxy.prop2.prop3 = "set2"; //setter test
+//     proxy.prop5 = "val4"; //setter test
+
+//     console.log(proxy);
+
+// })();
+
+// // LAB FINISh
+
+// var storyContext = window.story;
+// console.log(window.Native);
+
+// if (!storyContext) {
+//     storyContext = {
+//         accentColor: {
+//             color: "blue"
+//         }
+
+//     }
+// }
+
+// var logState = function () {
+//     if (!storyContext) {
+//         console.log("no storyContext");
+//         return
+//     }
+//     $("body").css("background-color", storyContext.accentColor.color);
+//     console.log(JSON.stringify(storyContext.State, 4));
+// }
+
+// var TestFunc2 = function () {
+//     if (!storyContext) {
+//         console.log("no context");
+//         return
+//     }
+//     let val = $("input[name='testInput2']").val();
+//     console.log(val);
+
+//     if (storyContext != undefined) {
+//         var result = context.TestMethod2(val);
+//         $("#result").val(result);
+//     }
+
+//     return result;
+// }
+
+// var Native = {
+//     TestCall1: function () {
+//         return 123;
+//     }
+// }
+
+// Native.TestCall1();
