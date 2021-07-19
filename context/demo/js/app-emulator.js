728a58bc4362a768db74e558dabe55b9ebf1cb9c
Demianenko Konstantin <dem.konst@yandex.ru>
2021-07-19 10:02:50 +0200

    ORG-45: atributes context demo presentation finish

---
+++ context/demo/js/app-emulator.js
@@ -0,0 +1,177 @@
+window._storyTemplate = {
+    "scheme": {
+        "app": {
+            "appMutable": false,
+            "contentMutable": false
+        },
+        "user": {
+            "appMutable": false,
+            "contentMutable": false
+        },
+        "presentation": {
+            "appMutable": false,
+            "contentMutable": false
+        },
+        "questProgress": {
+            "appMutable": true,
+            "contentMutable": false
+        },
+        "debugAppState": {
+            "appMutable": true,
+            "contentMutable": true
+        }
+    },
+    "app": {
+
+        "name": "context debug app",
+        "version": "0.1.2"
+    },
+    "user": {
+        "phone": "70000000001"
+    },
+    "presentation": {
+        "id": "209"
+    },
+    "questProgress": {
+        "stageName": "Изучение",
+        "state": "Success"
+    },
+    "debugAppState": {
+        "presentation": {
+            "notes": {
+                "text": "Тут большое или маленькое предложение, которое можно задать в конфигураторе"
+            },
+            "items": {
+                "field_1": {
+                    "value": "field_1 val",
+                    "order": 1
+                },
+                "field_3": {
+                    "value": "field_3 val 3",
+                    "order": 3
+                },
+                "field_2": {
+                    "value": "field_2 val 2",
+                    "order": 2
+                }
+            },
+            "rating": {
+                "visible": true,
+                "parameters": {
+                    "color": "#000000",
+                    "fontSize": 12
+                },
+                "value": 3
+
+            }
+        },
+        "f1": 0,
+        "f2": "v2",
+        "f3": true,
+        "f6": "f6 value",
+        "f8": "f8 value",
+        "f10": {
+            "f101": "value of f101"
+        },
+        "f14": "f14 to removeStoryProp",
+        "f51": {
+            "f51_test": "initial value"
+        }
+    }
+};
+
+window._storyTemplate = {
+    "scheme": {
+        "presentation": {
+            "appMutable": true,
+            "contentMutable": true
+        }
+    },
+    "presentation": {
+        "accentColor": {
+            "accentColorVisible": true,
+            "parameters": {
+                "color": "#555555",
+                "transparent": "#FF"
+            }
+        },
+        "form": {
+            "formVisible": true,
+            "items": {
+                "input_1": {
+                    "backgroundColor": "#ffffff",
+                    "fontColor": "#000000",
+                    "fontSize": 15,
+                    "inputValue": "some value",
+                    "name": "input name 1",
+                    "order": 1
+                },
+                "input_2": {
+                    "backgroundColor": "#123334",
+                    "fontColor": "#344445",
+                    "fontSize": 12,
+                    "inputValue": "",
+                    "name": "inputTitle 2",
+                    "order": 2
+                }
+            }
+        },
+        "notes": {
+            "notesVisible": true,
+            "parameters": {
+                "color": "#992222",
+                "fontSize": 40,
+                "text": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            }
+        },
+        "rating": {
+            "parameters": {
+                "color": "#6E4BD9",
+                "progress": 2,
+                "quantity": 4
+            },
+            "ratingVisible": true
+        }
+    }
+};
+
+window._set_story = function () {
+    window._story = _storyTemplate;
+    _onStoryChange();
+}
+
+window.nativeStory = {
+    setStoryProp: function (objectName, keyPath, _, value) {
+        let parts = keyPath.split('.');
+        let lastPart = parts.pop();
+        let newState = JSON.parse(JSON.stringify(_story))
+        let field = newState[objectName];
+        for (part of parts) {
+            if (field[part] === undefined || field[part] === null) {
+                field[part] = {}
+            }
+            field = field[part];
+            if (field === undefined) {
+                field = {};
+            }
+        }
+        field[lastPart] = value;
+        window._story = newState;
+        _onStoryChange();
+    },
+    deleteStoryProp: function (objectName, keyPath) {
+        let parts = keyPath.split('.');
+        let lastPart = parts.pop();
+        let newState = JSON.parse(JSON.stringify(_story))
+        let field = newState[objectName];
+        for (part of parts) {
+            if (field[part] === undefined || field[part] === null) {
+                field[part] = {}
+            }
+            field = field[part];
+        }
+        delete field[lastPart];
+        window._story = newState;
+        _onStoryChange();
+    }
+}
---
+++ context/demo/js/index.js
@@ -0,0 +1,203 @@
+; (function () {
+
+    window.logToDiv = function (value) {
+        $('#logs').append(`<div>${value}</div>`);
+    }
+    logToDiv(window.story);
+    //_set_story();
+
+    // Заголовок (header)
+    const renderHeader = () => {
+        if (window.story.presentation?.accentColor?.accentColorVisible) {
+            $("#header").removeClass('hidden');
+        } else {
+            $("#header").addClass('hidden');
+        }
+
+        if (window.story?.presentation?.accentColor?.accentColorVisible) {
+            var params = story.presentation.accentColor.parameters;
+            $("#header").css('background-color', params.color + params.transparent.substring(1));
+        }
+    }
+
+    // Текст (notes)
+    const renderNotes = () => {
+        if (window.story.presentation?.notes?.notesVisible) {
+            $("#notes-wrapper").removeClass('hidden');
+        } else {
+            $("#notes-wrapper").addClass('hidden');
+        }
+        let params = story.presentation.notes.parameters;
+        $('#notes').text(params.text);
+        $('#notes').css('font-size', params.fontSize);
+        $('#notes').css('color', params.color);
+    }
+
+    // Поля ввода (items)
+    const getFields = () => {
+        let fields = [];
+        for (let itemName in story.presentation.form.items) {
+            var item = story.presentation.form.items[itemName];
+            fields.push({
+                backgroundColor: item.backgroundColor,
+                fontColor: item.fontColor,
+                fontSize: item.fontSize,
+                name: itemName,
+                inputTitle: item.name,
+                inputValue: item.inputValue,
+                order: item.order
+            });
+        }
+        return fields.sort((a, b) => a.order - b.order);
+    }
+
+    let onChange = function (event) {
+        story.presentation.form.items[event.target.name].inputValue = event.target.value;
+    }
+
+    const updateField = (field) => {
+        $(`input[name='${field.name}'`).css('background-color', field.backgroundColor);
+        $(`input[name='${field.name}'`).css('color', field.fontColor);
+        $(`input[name='${field.name}'`).css('font-size', field.fontSize);
+        $(`input[name='${field.name}'`).change(onChange);
+        $(`input[name='${field.name}'`).val(field.inputValue);
+    }
+    let addFieldAndChangeListener = function (field) {
+        var input = $("#input-wrapper-container");
+        input.find("div span").text(field.inputTitle);
+        input.find("input").attr("name", field.name);
+        $("#text-input-list").append(input.html());
+        updateField(field)
+    }
+
+    const renderFields = () => {
+        if (window.story.presentation?.form?.formVisible) {
+            $("#form-wrapper").removeClass('hidden');
+        } else {
+            $("#form-wrapper").addClass('hidden');
+        }
+
+        var fields = getFields();
+        var currentFieldsCount = $("#text-input-list").children().length;
+
+        for (let i = 1; i <= Math.min(currentFieldsCount, fields.length); i++) {
+            let field = fields[i - 1];
+            updateField(field);
+        }
+
+        if (currentFieldsCount > fields.length) {
+            $("#text-input-list div:last-child").remove();
+        }
+
+        if (currentFieldsCount < fields.length) {
+            for (let i = 1; i <= fields.length; i++) {
+                let field = fields[i - 1];
+                if (i > currentFieldsCount) {
+                    addFieldAndChangeListener(field);
+                }
+            }
+        }
+    }
+
+    $("#btn-close").click(function () {
+        StoryCLM.Presentation.Close(2);
+    });
+
+    // Рейтинг
+    let setStars = function (quantity, progress) {
+        let color = window.story.presentation?.rating?.parameters?.color;
+        for (let i = 1; i <= 5; i++) {
+            if (i <= quantity) {
+                $(`#rating-star-${i}`).removeClass('hidden');
+            } else {
+                $(`#rating-star-${i}`).addClass('hidden');
+            }
+            $("#star-wrapper svg path").attr('stroke', color);
+            $("#star-wrapper svg").attr('fill', i <= progress ? color : 'none');
+            $(`label[for='star${i}']`).html($("#star-wrapper").html());
+        }
+    }
+
+    const renderRating = () => {
+        if (story.presentation.rating?.ratingVisible) {
+            $('#rating-root').removeClass('hidden');
+        } else {
+            $('#rating-root').addClass('hidden');
+        }
+
+        setStars(story.presentation.rating.parameters.quantity, story.presentation.rating.parameters.progress);
+
+        $("input[name='rate']").unbind('change');
+        $("input[name='rate']").change(function (e) {
+            story.presentation.rating.parameters.progress = e.target.value;
+        });
+    }
+
+    $("#btn-add").click(function () {
+        let fields = getFields();
+        let next = fields[fields.length - 1].order + 1;
+        let name = `input_${next}`;
+        var field = {
+            backgroundColor: '#FFF',
+            fontColor: '#000',
+            fontSize: 15,
+            name: name,
+            inputValue: '',
+            order: next
+        };
+        story.presentation.form.items[name] = field;
+
+        field.inputTitle = name;
+        fields.push(field);
+        addFieldAndChangeListener(field);
+    });
+
+    $("#btn-remove").click(function () {
+        let fields = getFields();
+
+        if (fields.length === 1) {
+            return;
+        }
+
+        var name = fields[fields.length - 1].name;
+        delete story.presentation.form.items[name];
+        $("#text-input-list div:last-child").remove();
+        fields.pop();
+    });
+
+    const render = () => {
+        renderHeader();
+        renderNotes();
+        renderFields();
+        renderRating();
+    }
+
+    const init = () => {
+
+        // story init
+        if (story.presentation === undefined) {
+            story.presentation = {};
+        }
+
+        if (story.presentation.form.items === undefined ||
+            story.presentation.form.items === null) {
+            story.presentation.form.items = {};
+
+        }
+
+        if (Object.keys(story.presentation.form.items).length === 0) {
+            story.presentation.items.field_1 = {
+                inputValue: "",
+                order: 1
+            }
+        }
+
+        render();
+    }
+
+    init();
+
+    story.onStoryChange = () => {
+        render();
+    }
+})();
